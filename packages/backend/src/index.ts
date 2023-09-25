91539181891c3eed646e68971691302f4a59b6a5
Dener-Silva <dener.siros@gmail.com>
2023-09-25 19:38:36 -0300

    Implemented Websockets

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,20 +1,29 @@
-import cors from 'cors'
-import express from 'express'
-
-import { Workspace } from 'types'
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import { WebSocketServer } from 'ws';
 
 const app = express()
 const port = 5000
+const server = http.createServer(app);
 
 app.use(cors({ origin: 'http://localhost:3000' }))
 
-app.get('/workspaces', (_, response) => {
-    const workspaces: Workspace[] = [
-        { name: 'api', version: '1.0.0' },
-        { name: 'types', version: '1.0.0' },
-        { name: 'web', version: '1.0.0' },
-    ]
-    response.json({ data: workspaces })
+app.get('/hello', (_, response) => {
+    response.json({ hello: "world" })
 })
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
+const wss = new WebSocketServer({ server });
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- packages/frontend/test/InputSystem.test.ts
+++ packages/frontend/test/InputSystem.test.ts
@@ -1,4 +1,4 @@
-import { onPointerMove, takeAllFromInputQueue } from "../src/InputSystem.js";
+import { onMouseDown, onMouseUp, onPointerMove, takeAllFromInputQueue } from "../src/InputSystem.js";
 
 global.window.innerWidth = 200;
 global.window.innerHeight = 200;
@@ -17,22 +17,22 @@ test.each([
 });
 
 test.each([
-    [0, 0, 3 * Math.PI / 4, 1],
-    [200, 200, -1 * Math.PI / 4, 1],
-])('Limit magnitude to 1 at x=%i y=%i', (pageX, pageY, angle, magnitude) => {
+    [0, 0, 3 * Math.PI / 4],
+    [200, 200, -Math.PI / 4],
+])('Limit magnitude to 1 at x=%i y=%i', (pageX, pageY, angle) => {
     onPointerMove({ pageX, pageY } as PointerEvent);
 
     const inputQueue = takeAllFromInputQueue();
 
     expect(inputQueue.length).toBe(1);
     expect(inputQueue[0].angle).toBe(angle);
-    expect(inputQueue[0].magnitude).toBe(magnitude);
+    expect(inputQueue[0].magnitude).toBe(1);
 });
 
 test('Should take all inputs from queue', () => {
     onPointerMove({ pageX: 0, pageY: 1 } as PointerEvent);
-    onPointerMove({ pageX: 2, pageY: 3 } as PointerEvent);
-    onPointerMove({ pageX: 4, pageY: 5 } as PointerEvent);
+    onMouseDown();
+    onMouseUp();
 
     expect(takeAllFromInputQueue()).toHaveLength(3);
     expect(takeAllFromInputQueue()).toHaveLength(0);
