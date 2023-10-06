612852a54300318937f1ed10869779ea384fc138
Dener-Silva <dener.siros@gmail.com>
2023-10-06 01:47:18 -0300

    SSL support

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,15 +1,33 @@
 import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import { AddressInfo, WebSocketServer } from 'ws';
 import { runWebSocketServer } from './webSocketServer.js';
 import dotenv from 'dotenv';
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'production'}` })
 
-const port = Number(process.env.PORT);
-const server = http.createServer(function (_req, res) {
+function sayHello(_req: any, res: any) {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(`{ "time": "${new Date().toLocaleString()}"}`);
+    res.write(`{ "time": "${new Date().toISOString()}"}`);
     res.end();
-});
+}
+
+function createServer() {
+    if (process.env.SSL_CERT_PATH && process.env.SSL_KEY_PATH) {
+        console.log('SSL is enabled');
+        return https.createServer({
+            cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+            key: fs.readFileSync(process.env.SSL_KEY_PATH)
+        }, sayHello);
+    } else {
+        console.log('SSL is disabled');
+        return http.createServer(sayHello);
+    }
+}
+
+const port = Number(process.env.PORT);
+const server = createServer();
+
 
 // TODO: check origin
 runWebSocketServer(new WebSocketServer({ server }));
